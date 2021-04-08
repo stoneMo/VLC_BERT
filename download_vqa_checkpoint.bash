3bb02feecc08e93a9e91673e58156eace01ee84c
ShentongMo <shentonm@andrew.cmu.edu>
2021-04-08 11:23:03 +0800

    init

--- download_vqa_checkpoint.bash
+++ download_vqa_checkpoint.bash
@@ -22,4 +22,3 @@ filename="vqa_fine_tuned.th"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
 
-
