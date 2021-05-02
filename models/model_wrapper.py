7dc3a37c8c2d752e98deb86f373f84a6beaf88b6
ShentongMo <shentonm@andrew.cmu.edu>
2021-05-03 02:11:25 +0800

    init

--- models/model_wrapper.py
+++ models/model_wrapper.py
@@ -76,7 +76,7 @@ class ModelWrapper():
         # print("sequence_output:", output_dict["sequence_output"].shape)
         # print("pooled_output:", output_dict["pooled_output"].shape)
         print("logits:", output_dict["logits"].shape)                      # [48, 141, 30522]
-        print("logits:", output_dict["logits"][0,:,:2)                      # [48, 141, 30522]
+        print("logits:", output_dict["logits"][0,:,:2])                     # [48, 141, 30522]
         print("seq_relationship_score:", output_dict["seq_relationship_score"].shape)   # [48, 2]
 
         print("==========after model==========")
